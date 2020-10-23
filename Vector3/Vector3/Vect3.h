31572c416772bdd2f6066bfd5c1b956e0490323f
XavierMacias <xmaciasxicola@gmail.com>
2020-10-23 18:16:05 +0200

    finish

--- Vector3/Vector3/Vect3.h
+++ Vector3/Vector3/Vect3.h
@@ -1,40 +1,75 @@
 #pragma once
 #include <math.h> 
 
+template <class T>
 class Vect3
 {
 	public:
 
-		Vect3();
-		Vect3(float a, float b, float c);
-		Vect3(const Vect3& c);
-		~Vect3();
+		Vect3() {
+			x = 0;
+			y = 0;
+			z = 0;
+		}
 
-		float getFirst() const;
-		float getSecond() const;
-		float getThird() const;
+		Vect3(T a, T b, T c) {
+			x = a;
+			y = b;
+			z = c;
+		}
 
-		Vect3 normalize() const;
-		float distance_to(const Vect3& vect);
-		float dot_product(const Vect3& vect);
-		Vect3 cross_product(const Vect3& vect);
-		float angle_between(const Vect3& vect);
+		Vect3(const Vect3& c) {
+			x = c.x;
+			y = c.y;
+			z = c.z;
+		}
 
-		Vect3 operator + (Vect3 const& obj) {
+		float getX() const { return x; }
+		float getY() const { return y; }
+		float getZ() const { return z; }
+
+		void normalize() {
+			T denom = sqrt(x*x + y*y + z*z);
+			x = x / denom; 
+			y = y / denom;
+			z = z / denom;
+		}
+
+		T distance_to(const Vect3& vect) const {
+			T x1 = x - vect.x;
+			T y1 = y - vect.y;
+			T z1 = z - vect.z;
+
+			return sqrt(x1*x1 + y1*y1 + z1*z1);
+		}
+
+		T dot_product(const Vect3& vect) const {
+			return x * vect.x + y * vect.y + z * vect.z;
+		}
+		
+		Vect3 cross_product(const Vect3<T>& vect) const {
+			T x1 = y * vect.z - vect.y * z;
+			T y1 = z * vect.x - vect.z * x;
+			T z1 = x * vect.y - vect.x * y;
+
+			return Vect3<T>(x1, y1, z1);
+		}
+		
+		float angle_between(const Vect3& vect) const {
+			return acos(dot_product(vect) / (sqrt(x*x + y*y + z*z) * sqrt(vect.x*vect.x + vect.y*vect.y + vect.z*vect.z)));
+		}
+
+		Vect3 operator + (Vect3& const obj) {
 			Vect3 res;
-			res.first = first + obj.first;
-			res.second = second + obj.second;
-			res.third = third + obj.third;
+			res.x = x + obj.x;
+			res.y = y + obj.y;
+			res.z = z + obj.z;
 			return res;
 		}
 
 
 	private:
-		float first, second, third;
+		T x, y, z;
 		
 };
-
-inline float Vect3::getFirst() const { return first; }
-inline float Vect3::getSecond() const { return second; }
-inline float Vect3::getThird() const { return third; }
 
--- Vector3/Vector3/main.cpp
+++ Vector3/Vector3/main.cpp
@@ -7,32 +7,22 @@
 int main()
 {
     //std::cout << "Hello World!\n";
-    Vect3 a = Vect3(2.0f,7.0f,3.0f);
-    Vect3 b(a);
-    std::cout << "Vec: " << b.getSecond() << std::endl;
-    Vect3 f = Vect3(1.0f, 0.0f, 3.0f);
-    Vect3 c = b + a;
+    Vect3<float> a = Vect3<float>(2.0f,7.0f,3.0f);
+    Vect3<float> b(a);
+    std::cout << "Vec: " << b.getY() << std::endl;
+    Vect3<float> f = Vect3<float>(1, 0, 3);
+    Vect3<float> c = b + a;
 
-    std::cout << "VecC: " << c.getSecond() << std::endl;
+    std::cout << "VecC: " << c.getY() << std::endl;
 
-    Vect3 d = a.normalize();
-    std::cout << "VecD: x: " << d.getFirst() << ", y: " << d.getSecond() << ", z: " << d.getThird() << std::endl;
+    a.normalize();
+    std::cout << "VecD: x: " << a.getX() << ", y: " << a.getY() << ", z: " << a.getZ() << std::endl;
 
     std::cout << "DISTANCE: " << a.distance_to(c) << std::endl;
     std::cout << "DOT PRODUCT: " << a.dot_product(c) << std::endl;
 
-    Vect3 e = a.cross_product(f);
-    std::cout << "VecE: x: " << e.getFirst() << ", y: " << e.getSecond() << ", z: " << e.getThird() << std::endl;
+    Vect3<float> e = a.cross_product(f);
+    std::cout << "VecE: x: " << e.getX() << ", y: " << e.getY() << ", z: " << e.getZ() << std::endl;
 
     std::cout << "ANGLE: " << a.angle_between(f) << std::endl;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
